fded339860af419a288ddd32facaa29054946269
ap0phasi <66261738+ap0phasi@users.noreply.github.com>
2023-11-12 16:37:36 +1000

    API and RabbitMQ Flexibility
    Updated backend to allow for both RabbitMQ and FastAPI workers, to avoid losing FastAPI functionality.

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import uuid
 import aiormq
 import aiormq.abc
 import json
+import aiohttp
 
 import numpy as np
 import pandas as pd
@@ -46,6 +47,7 @@ class BackendRpcClient:
 
         await self.channel.basic_publish(
             request_json.encode(),
+            # Different workers are looking at different queues, we write to the appropiate queue here
             routing_key=json.loads(request_json).get('request_to')+'_rpc',
             properties=aiormq.spec.Basic.Properties(
                 content_type='application/json',
@@ -57,11 +59,30 @@ class BackendRpcClient:
         response_json = await future
         return json.loads(response_json.decode())
     
-# Make request
+# Make request to RPC
 async def make_request(message_json):
     backend_rpc = await BackendRpcClient().connect()
     response = await backend_rpc.call(message_json)
     return response
+
+# Make request to API
+async def send_request(url, params = None, json = None):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params = params, json = json) as resp:
+            response = await resp.json()
+            return response
+        
+# Set API URLS
+url_routes = {
+    "delta" : "http://deltaworker:8002/",
+    "duck" : "http://duckworker:8003/"
+}
+
+# Set message routes
+message_routes = {
+    "delta" : "rpc",
+    "duck" : "rpc"
+}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -75,8 +96,23 @@ async def websocket_endpoint(websocket: WebSocket):
         message_text = await websocket.receive_text()
         # Extract JSON
         message_json = json.loads(message_text)
-        # Call RPC and await response
-        response_message = await backend_rpc.call(json.dumps(message_json))
-        # I am confused why I need to dump the json, shouldn't the response already be that?
-        json_response = json.dumps(response_message, indent=4)
-        await websocket.send_text(json_response)
+        
+        if message_routes[message_json.get('request_to')] == "rpc":
+            # Call RPC and await response
+            response_message = await backend_rpc.call(json.dumps(message_json))
+            # I am confused why I need to dump the json, shouldn't the response already be that?
+            json_response = json.dumps(response_message, indent=4)
+            await websocket.send_text(json_response)
+            
+        elif message_routes[message_json.get('request_to')] == "api":
+            message_requestto = message_json.get('request_to')
+            message_requestendpoint = message_json.get('request_endpoint')
+
+            # Attempt to send the request and get a response
+            json_response = await send_request(
+                url_routes[message_requestto] + message_requestendpoint,
+                json=message_json.get('request_args')
+            )
+
+            # Send the JSON response data
+            await websocket.send_text(json_response)
